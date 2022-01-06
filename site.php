276069193c6149fccfa523fb45732fb825a0bf47
Mohammed Ziyan Aslam <74085603+MohammedZiyanAslam@users.noreply.github.com>
2022-01-05 22:31:20 -0500

    Add files via upload

---
+++ site.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+      <?php
+        echo ("<h1> Hello World </h1>");
+        echo "<hr>";
+        echo ("<h2> This is my first php code </h2>");
+        echo "<pr> This is Mohammad's website </pr>";
+      ?>
+
+  </body>
+</html>
